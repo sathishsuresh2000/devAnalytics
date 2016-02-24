ffd6c7353757ac49f81568cf173dbfdaed19bb93
Suresh, Sathish (ESI) <sathish.suresh@adp.com>
2016-02-24 20:48:59 +0530

    updated the right path

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,13 @@
 "use strict";
 
 var express = require("express"),
-  router = express.Router();
+  router = express.Router(),
+  path=require("path");
+
 var inventoryReportRouter = require("./inventoryReportRoute");
 
 router.get("/", function (req, res) {
-  res.sendFile('public/index.html');
+  res.sendFile(path.join(__dirname,'../public/index.html'));
 });
 
 router.use("/report", inventoryReportRouter);
